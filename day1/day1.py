1a154c038289e29b7fb7610f5447e0eb958824c6
Leon Bauer <lbauer822@gmail.com>
2020-12-01 15:15:29 -0500

    first commit

---
+++ day1/day1.py
@@ -0,0 +1,17 @@
+inputfile = open("D:\coding\Advent of Code 2020\day1\day1input.txt", "r")
+numlist = []
+lines = inputfile.readlines()
+endlines = []
+for value in lines:
+    endlines.append(int(value.strip("\n")))
+
+def day1(numbers):
+    answer = 0
+    for i in numbers:
+        for j in numbers:
+            if (i + j) == 2020:
+                 answer = i * j
+                 return answer
+    return answer
+
+print(day1(endlines))
---
+++ day1/day1part2.py
@@ -0,0 +1,17 @@
+inputfile = open("D:\coding\Advent of Code 2020\day1\day1input.txt", "r")
+numlist = []
+lines = inputfile.readlines()
+endlines = []
+for value in lines:
+    endlines.append(int(value.strip("\n")))
+
+def day1(numbers):
+    answer = 0
+    for i in numbers:
+        for j in numbers:
+            for k in numbers:
+                if (i + j + k) == 2020:
+                    answer = i * j * k
+    return answer
+
+print(day1(endlines))
