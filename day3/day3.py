b71e876b9c6edbbb15f5d661c1e037fc3c873a12
Leon Bauer <lbauer822@gmail.com>
2020-12-03 01:19:09 -0500

    Day 3 of Advent of Code 2020

---
+++ day3/day3.py
@@ -0,0 +1,35 @@
+import math
+
+inputfile = open("D:\coding\Advent of Code 2020\day3\day3input.txt", "r")
+lines = inputfile.readlines()
+multiplier = 0
+down = 0
+right = 0
+slopemap = []
+extendedmap = []
+
+for value in lines:
+    slopemap.append(value.strip("\n"))
+
+height = len(slopemap)
+
+def day3(map, down, across):
+    treecount = 0
+    rightindex = 0
+    downindex = 0
+    requiredwidth = across * height
+    multiplier = math.ceil(requiredwidth / len(slopemap[0]))
+    for line in map:
+        extendedmap.append(line * multiplier)
+    
+    for line in extendedmap:
+        if downindex != len(extendedmap) - 1:
+            if extendedmap[downindex + down][rightindex + across] == "#":
+                rightindex += across
+                downindex += down
+                treecount += 1
+            else:
+                rightindex += across
+                downindex += down
+    return treecount
+print(day3(slopemap, 1, 3))
---
+++ day3/day3part2.py
@@ -0,0 +1,37 @@
+import math
+
+inputfile = open("D:\coding\Advent of Code 2020\day3\day3input.txt", "r")
+lines = inputfile.readlines()
+multiplier = 0
+slopemap = []
+
+for value in lines:
+    slopemap.append(value.strip("\n"))
+
+height = len(slopemap)
+
+def day3(map, down, across):
+    requiredwidth = 0
+    multiplier = 0
+    treecount = 0
+    rightindex = 0
+    downindex = 0
+    extendedmap = []
+    requiredwidth = across * height
+    multiplier = math.ceil(requiredwidth / len(slopemap[0]))
+    for line in map:
+        extendedmap.append(line * multiplier)
+
+    for line in extendedmap:
+        if downindex != len(extendedmap) - 1:
+            if extendedmap[downindex + down][rightindex + across] == "#":
+                rightindex += across
+                downindex += down
+                treecount += 1
+            else:
+                rightindex += across
+                downindex += down
+    return treecount
+
+
+print(day3(slopemap, 1, 1) * day3(slopemap, 1, 3) * day3(slopemap, 1, 5) * day3(slopemap, 1, 7) * day3(slopemap, 2, 1))
