3ef28c3365d67469dda331c1d21564efa852921b
Leon Bauer <lbauer822@gmail.com>
2020-12-06 00:48:18 -0500

    Advent of Code 2020 Day 6

---
+++ day6/day6.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+inputfile = open("D:\coding\Advent of Code 2020\day6\day6input.txt", "r")
+with inputfile as f:
+    lines = f.read()
+forms = lines.split("\n\n")
+
+cleanedforms = []
+for p in forms:
+    cleanedforms.append(p.split("\n"))
+
+def day6(formlist):
+    lettercount = 0
+    for group in formlist:
+        chardict = {}
+        for person in group:
+            for letter in person:
+                keys = chardict.keys()
+                if letter in keys:
+                    chardict[letter] += 1
+                else:
+                    chardict[letter] = 1
+        for letter in chardict:
+            lettercount += 1
+    return lettercount
+
+print(day6(cleanedforms))
+
---
+++ day6/day6part.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+inputfile = open("D:\coding\Advent of Code 2020\day6\day6input.txt", "r")
+with inputfile as f:
+    lines = f.read()
+forms = lines.split("\n\n")
+
+cleanedforms = []
+for p in forms:
+    cleanedforms.append(p.split("\n"))
+
+def day6(formlist):
+    lettercount = 0
+    for group in formlist:
+        chardict = {}
+        for person in group:
+            for letter in person:
+                keys = chardict.keys()
+                if letter in keys:
+                    chardict[letter] += 1
+                else:
+                    chardict[letter] = 1
+        for letter in chardict:
+            if chardict[letter] == len(group):
+                lettercount += 1
+    return lettercount
+
+testlist = [["abc"], ["a", "b", "c"], ["ab", "ac"], ["a", "a", "a", "a"], ["b"]]
+print(day6(cleanedforms))
